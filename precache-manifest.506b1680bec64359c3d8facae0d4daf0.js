33865b0fcb44544dad77916bd7947294ccee7bbf
Garvit Saxena <garvit.saxena@scool.in>
2020-06-19 23:50:54 +0530

    Updates

---
+++ precache-manifest.506b1680bec64359c3d8facae0d4daf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085556fbec79d839dcfe462c68f27af6",
+    "url": "/User-Registration-App/index.html"
+  },
+  {
+    "revision": "33d5cc395b91ef28884a",
+    "url": "/User-Registration-App/static/css/main.4f9a22c7.chunk.css"
+  },
+  {
+    "revision": "41942602734bf11e4312",
+    "url": "/User-Registration-App/static/js/2.807f1df4.chunk.js"
+  },
+  {
+    "revision": "dfca7d12382cdd2961240b2d18b75b38",
+    "url": "/User-Registration-App/static/js/2.807f1df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d5cc395b91ef28884a",
+    "url": "/User-Registration-App/static/js/main.325aa9e2.chunk.js"
+  },
+  {
+    "revision": "d9655a7b8e6537ecb179",
+    "url": "/User-Registration-App/static/js/runtime-main.8a875407.js"
+  },
+  {
+    "revision": "e8c98604d037d650cb5ca3b04a6432e0",
+    "url": "/User-Registration-App/static/media/background.e8c98604.jpg"
+  },
+  {
+    "revision": "2c6e94aaf20d66610132b533ae100324",
+    "url": "/User-Registration-App/static/media/profile.2c6e94aa.png"
+  }
+]);
